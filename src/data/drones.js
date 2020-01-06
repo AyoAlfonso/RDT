9b71248f2aa7b53c4102c26429c89cc658e7f802
AyoAlfonso <alfonsoayo7@gmail.com>
2020-01-06 23:18:00 +0100

    Add Drone Fleet dummy data

---
+++ src/data/drones.js
@@ -0,0 +1,48 @@
+let registeredDevices = [
+    {
+        area: 1,
+        deviceID: 'Resin.Drone.a2345.aaa',
+        group: 'aaa',
+    },
+    {
+        area: 2,
+        deviceID: 'Resin.Drone.b2345.aaa',
+        group: 'aaa',
+    },
+    {
+        area: 3,
+        deviceID: 'Resin.Drone.c2345.aaa',
+        group: 'aaa',
+    },
+    {
+        area: 1,
+        deviceID: 'Resin.Drone.d2345.bbb',
+        group: 'bbb',
+    },
+    {
+        area: 2,
+        deviceID: 'Resin.Drone.e2345.bbb',
+        group: 'bbb',
+    },
+    {
+        area: 3,
+        deviceID: 'Resin.Drone.f2345.bbb',
+        group: 'bbb',
+    },
+    {
+        area: 1,
+        deviceID: 'Resin.Drone.g2345.ccc',
+        group: 'ccc',
+    },
+    {
+        area: 2,
+        deviceID: 'Resin.Drone.h2345.ccc',
+        group: 'ccc',
+    },
+    {
+        area: 3,
+        deviceID: 'Resin.Drone.i2345.ccc',
+        group: 'ccc',
+    },
+]
+module.exports = registeredDevices
